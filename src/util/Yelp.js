8cf85fd998e82cf68487e957bcdc1a4695e0cd86
Feifx <32243939+faysoserious@users.noreply.github.com>
2022-02-13 22:01:43 +0100

    hide the api key

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,9 +1,13 @@
-const apiKey = 'WMgK81yWPik55wEK0eQG4fNLa8PHyaXaiQ18vWlsufeNt-xaSwG15epFMaf4NEIhjHdotFjm55w9RyYQ-M0Xmi2MavfNsnVkgTafuw8pFWf2R6_xF2NOL4MINxX1YXYx';
+import React from 'react';
+
+
+
+console.log(process.env.REACT_APP_API_KEY);
 export const Yelp = {
   search(term, location, sortBy) {
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
       headers: {
-        Authorization: `Bearer ${apiKey}`
+        Authorization: `Bearer ${process.env.REACT_APP_API_KEY}`
       }
     }).then(response => {
       return response.json();
